867dddf5db87f60b02f52e682de283b7a2801207
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-09-19 17:18:04 +0700

    Solving conflict

--- app/src/main/java/com/fdev/instagramclone/framework/presentation/main/MainActivity.kt
+++ app/src/main/java/com/fdev/instagramclone/framework/presentation/main/MainActivity.kt
@@ -2,9 +2,6 @@ package com.fdev.instagramclone.framework.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.lifecycle.Observer
-=======
 import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.navigation.NavHost
@@ -12,16 +9,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
->>>>>>> master
 import com.fdev.instagramclone.R
 import com.fdev.instagramclone.databinding.ActivityAuthBinding
 import com.fdev.instagramclone.databinding.ActivityMainBinding
 import com.fdev.instagramclone.util.SessionManager
-<<<<<<< HEAD
-=======
 import com.fdev.instagramclone.util.printLogD
 import com.google.android.material.bottomnavigation.BottomNavigationView
->>>>>>> master
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -29,6 +22,8 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     private var _binding : ActivityMainBinding? = null
+
+
 
     private val binding
         get() = _binding!!
@@ -41,14 +36,13 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-<<<<<<< HEAD
+
 
         sessionManager.currentUser.observe(this, Observer {
-            binding.testUser.setText("Welcome ${it?.username}")
         })
 
-    }
-=======
+
+
         setUpNavigation()
         sessionManager.currentUser.observe(this, Observer {
         })
@@ -64,5 +58,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 
->>>>>>> master
+
 }
