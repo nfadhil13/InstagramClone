095778963934323870e242465e0813b3f6fbe951
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-09-21 12:37:21 +0700

    Add profile

--- app/src/main/java/com/fdev/instagramclone/framework/presentation/main/account/ProfileContainerFragment.kt
+++ app/src/main/java/com/fdev/instagramclone/framework/presentation/main/account/ProfileContainerFragment.kt
@@ -4,25 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.fdev.instagramclone.R
-import com.fdev.instagramclone.databinding.FragmentHomeBinding
-import com.fdev.instagramclone.databinding.FragmentProfileBinding
+import com.fdev.instagramclone.databinding.FragmentProfileContainerBinding
 import com.fdev.instagramclone.framework.presentation.main.BaseMainFragment
 
-class ProfileFragment : BaseMainFragment(){
+class ProfileContainerFragment : BaseMainFragment(){
 
-    private var _binding : FragmentProfileBinding? = null
+    private var _binding : FragmentProfileContainerBinding? = null
 
     private val binding
         get() = _binding!!
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentProfileBinding.inflate(inflater , container , false)
+        _binding = FragmentProfileContainerBinding.inflate(inflater , container , false)
         return binding.root
     }
 
